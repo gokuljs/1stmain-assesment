485b6690af0c7b19a1e9658a86c0a13b25b70d0b
gokuljs <gokuljs029@gmail.com>
2021-10-30 06:26:05 +0530

    test.py

--- test.py
+++ test.py
@@ -12,13 +12,13 @@ def solve(final):
         if i==50: # when you get a 50 dollar u decerement the value of 25 and increment value of 50 by 1
             a25=a25-1
             a50=a50+1
-        if i==100:
-            if(a50==0 and a25>3):
+        if i==100: # case when you have an 100$ bill 
+            if(a50==0 and a25>3): # so when you have dont 50 dollar and you have more than 3 25 dollar bills
                 a25=a25-3
-            else:
+            else:  # case where you have more than 1 50 dollar bill and 25$ bill 
                 a25=a25-1
                 a50=a50-1
-            if a25<0 or a50<0:
+            if a25<0 or a50<0: # case where you have 100$ and no money to return 
                 return "NO"
     return "YES"
         
